60f08d4c59123ad2127d075d933a0c3cc545656b
AngeloVSO <angelo_odwyer@hotmail.com>
2023-04-19 00:40:54 -0300

    Adiciona rotas da api de login e cadastro usuario

--- src/app/api/salcity.api.ts
+++ src/app/api/salcity.api.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Card } from '../models/card';
+import { EMPTY, catchError } from 'rxjs';
 
 @Injectable()
 export class SalcityApiService {
@@ -12,5 +13,43 @@ export class SalcityApiService {
 
   getCardByName(cardName: string) {
     return this.http.get(`https://salcity.vercel.app/cards/${cardName}`);
+  }
+
+  login(body: { username: string; password: string }) {
+    return this.http
+      .post('https://salcity.vercel.app/auth/login', body, {
+        observe: 'response',
+      })
+      .pipe(
+        catchError((error) => {
+          if (error) {
+            alert(error.error.error);
+            return EMPTY;
+          }
+
+          throw error;
+        })
+      );
+  }
+
+  addCard(body: { name: string; imageUrl: string }) {
+    const token =
+      '.eyJpZCI6ImFkZTVjYjRhLWI5YzItNDFjYS04MzM4LWEwMGM4NGFjOGJmZSIsInJvbGUiOiJzQUxkbUluY2l0eSIsImlhdCI6MTY4MTg3MDUzMX0.HXglca_3d_PfKT1XDH9eOUqekZ1uhRzsg1NSiB3pwD0';
+    const header = new HttpHeaders().set('Authorization', `bearer ${token}`);
+
+    return this.http
+      .post('https://salcity.vercel.app/cards', body, {
+        headers: header,
+      })
+      .pipe(
+        catchError((error) => {
+          if (error.error.error == 'Token inválido') {
+            alert('Usuário não autorizado a adicionar carta.');
+            return EMPTY;
+          }
+
+          throw error;
+        })
+      );
   }
 }
